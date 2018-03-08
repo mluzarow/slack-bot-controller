d6584bba920c91821ccaf56b040986d3e9f1320d
Mackenzie <ma.luzarow@gmail.com>
2018-03-07 19:55:10 -0600

    Funky autoloader maybe

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 require ('src/TaskSlammer.php');
+require ('src/tasks/TestCommand.php');
 
-function req_auto ($name) {
-	require ('src/tasks/'.$name.'.php');
-}
-spl_autoload_register ('req_auto');
+// function req_auto ($name) {
+// 	require ('src/tasks/'.$name.'.php');
+// }
+// spl_autoload_register ('req_auto');
 
 if (!empty($_POST)) {
 	$tasker = new TaskSlammer ($_POST);
