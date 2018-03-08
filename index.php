1a98e37bbbd5f3e79b725577ffccb5f96725b53a
Mackenzie <ma.luzarow@gmail.com>
2018-03-07 18:51:16 -0600

    Some response

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+http_response_code(200);
+header('Content-Type: application/json');
+header('Status: 200 OK');
+
+$response = array(
+	'status' => true,
+	'message' => 'Some message'
+);
+
+echo json_encode($response);
