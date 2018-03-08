88001fade0d080dac3ba10d8375b0561803d36f0
Mackenzie <ma.luzarow@gmail.com>
2018-03-07 21:57:43 -0600

    Time out on no POST.

--- index.php
+++ index.php
@@ -9,11 +9,6 @@ require ('src/tasks/TestCommand.php');
 
 if (!empty($_POST)) {
 	$tasker = new TaskSlammer ($_POST);
-} else {
-	http_response_code(400);
-	header('Content-Type: text/html; charset=UTF-8');
-	header('Status: 400 Bad Request');
-	echo 'No POST data found.';
 }
 
 // http_response_code(200);
