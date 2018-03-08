6d7925eb4392dbc7c20012e87e8a4fba4a84ab70
Mackenzie <ma.luzarow@gmail.com>
2018-03-07 22:05:50 -0600

    Autoloader

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
 require ('src/TaskSlammer.php');
-require ('src/tasks/TestCommand.php');
+// require ('src/tasks/TestCommand.php');
 
-// function req_auto ($name) {
-// 	require ('src/tasks/'.$name.'.php');
-// }
-// spl_autoload_register ('req_auto');
+function req_auto ($name) {
+	require ('src/tasks/'.$name.'.php');
+}
+spl_autoload_register ('req_auto');
 
 if (!empty($_POST)) {
 	$tasker = new TaskSlammer ($_POST);
 }
-
-// http_response_code(200);
-// header('Content-Type: application/json');
-// header('Status: 200 OK');
-// 
-// $response = array(
-// 	'status' => true,
-// 	'message' => 'Some message'
-// );
-// 
-// echo json_encode($response);
