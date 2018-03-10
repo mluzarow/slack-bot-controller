4cc301315109a7cac91abd9c2f5f29863ef3b0fd
Mackenzie <ma.luzarow@gmail.com>
2018-03-10 11:40:01 -0600

    Yeah

--- src/tasks/AnimeLookup.php
+++ src/tasks/AnimeLookup.php
@@ -25,15 +25,15 @@ class AnimeLookup extends GenericTask {
 			return ('Command requires argument in the form of a query. E.G. /anime black lagoon');
 		}
 		
-		$this->api_url = 'http://api.jikan.me/search/anime/"'.$string.'"/1';
+		$this->api_url = 'http://api.jikan.me/search/anime/'.$string.'/1';
 	}
 	
 	private function requestAnimeList () {
 		$curl = curl_init ();
 		
 		curl_setopt ($curl, CURLOPT_URL, $this->api_url);
-		curl_setopt ($curl, CURLOPT_POST, 0);
-		curl_setopt ($curl, CURLOPT_POSTFIELDS, '');
+		// curl_setopt ($curl, CURLOPT_POST, 0);
+		// curl_setopt ($curl, CURLOPT_POSTFIELDS, '');
 		curl_setopt ($curl, CURLOPT_RETURNTRANSFER, true);
 		
 		$response = curl_exec ($curl);
